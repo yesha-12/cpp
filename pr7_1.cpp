92fb59b0bcc5b94464b4b7393702b632371e5e9f
yesha-12 <25cs032@charusat.edu.com>
2026-04-15 18:47:52 +0530

    modify

---
+++ pr7_1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class cal
+{
+    public:
+    int add(int a,int b)
+    {
+        return a+b;
+    }
+     int add(int a,int b,int c)
+    {
+        return a+b+c;
+    }
+    float add(float a,float b)
+    {
+        return a+b;
+    }
+    float add(float a,int b)
+    {
+        return a+b;
+    }
+};
+int main ()
+{
+    cal c;
+    int r1=c.add(3,4);
+    int r2=c.add(3,4,5);
+    float r3=c.add(3.5f,6.7f);
+    float r4=c.add(3.2f,4);
+    cout<<"both int:"<<r1<<"\nthree int:"<<  r2<<"\nboth float:"<< r3<< "\none int one float :"<<r4;
+    return 0;
+}
---
+++ pr7_2.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+    private:
+    int real,image;
+    public:
+    complex ()
+    {
+        real=0;
+        image=0;
+    }
+    complex(int r,int i)
+    {
+        real=r;
+        image=i;
+
+    }
+    complex operator+(complex c)
+    {
+        complex temp;
+        temp.real=real+c.real;
+        temp.image=image+c.image;
+        return temp;
+    }
+     complex operator-(complex c)
+    {
+        complex temp;
+        temp.real=real-c.real;
+        temp.image=image-c.image;
+        return temp;
+    }
+
+    void display()
+    {
+        cout<<real<<"+"<<image<<"i"<<endl;
+    }
+};
+int main()
+{
+    complex c1(3,4),c2(4,5);
+    complex c3 = c1+c2;
+    complex c4 = c2-c1;
+
+    cout << "First Complex Number: ";
+    c1.display();
+
+    cout << "Second Complex Number: ";
+    c2.display();
+
+    cout << "Addition Result: ";
+    c3.display();
+
+    cout << "Subtraction Result: ";
+    c4.display();
+    return 0;
+}
